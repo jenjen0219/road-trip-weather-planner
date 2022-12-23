88d8b2067e37dac994c01273130a7d26de58ea4b
[Jen ny] <[jengutierrez.1210@gmail.com]>
2022-12-22 17:20:58 -0800

    completed the test for the mapping api and started the loop that will eventually be used to run all of our functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,20 +42,22 @@ function cityCoordinates() {
         });
 
 
+
+
 };
 var test = document.getElementById("save-btn");
 
 //in this function we are going to fetch the api response that we will use to give a weather forecast for each city
-function cityWeather() {
+function cityWeather(event) {
+    event.preventDefault();
 
-    var departDate = document.getElementById("departure-date").val();
+    var departDate = document.getElementById("departure-date").value;
 
     //this is rearranging our date to the format accepetted by the weather api 
-    const unformattedDate = departDate.split('/');
-    const formattedDate = unformattedDate[2] + "-" + unformattedDate[0] + "-" + unformattedDate[1];
+
     //i think we might have to do some work to translate the date variable into either UNIX time (that is, seconds since midnight GMT on 1 Jan 1970) or a string formatted as follows: [YYYY]-[MM]-[DD]T[HH]:[MM]:[SS][timezone].
     // var lengthOfStay = document.getElementById("");
-    var date = formattedDate + 'T12:00:00';
+    var date = departDate + 'T12:00:00';
     var APIKey = '30f72d6795msh68cc89a67fcb9e7p1c5c49jsn45bfacd7bd58';
 
     var lonCoord = -122.335167;
@@ -79,10 +81,10 @@ function cityWeather() {
     fetch('https://dark-sky.p.rapidapi.com/' + latCoord + ',' + lonCoord + ',' + date + '?units=uk2', options)
         .then(response => response.json())
         .then(data => {
-            tempMin = daily.data[0].temperatureMin;
-            tempMax = daily.data[0].temperatureMax;
-            humidity = daily.data[0].humidity;
-            windSpeed = daily.data[0].windspeed;
+            tempMin = data.daily.data[0].temperatureMin;
+            tempMax = data.daily.data[0].temperatureMax;
+            humidity = data.daily.data[0].humidity;
+            windSpeed = data.daily.data[0].windSpeed;
 
             console.log(tempMin, tempMax, humidity, windSpeed);
 
@@ -91,9 +93,52 @@ function cityWeather() {
 
 
 
-}
+};
 
-test.addEventListener('click', cityWeather);
+
+function cityMapping(event) {
+    event.preventDefault();
+    var lonCoord = -122.335167;
+    var latCoord = 47.608013;
+
+    var coord = latCoord + ',' + lonCoord
+    var testest;
+
+    var origin = coord;
+    var destinations = '40.630099,-73.993521;40.644895,-74.013818;40.627177,-73.980853';
+    var testduration;
+    var testdistance;
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '30f72d6795msh68cc89a67fcb9e7p1c5c49jsn45bfacd7bd58',
+            'X-RapidAPI-Host': 'trueway-matrix.p.rapidapi.com'
+        }
+    };
+
+    fetch('https://trueway-matrix.p.rapidapi.com/CalculateDrivingMatrix?origins=' + origin + '&destinations=' + destinations, options)
+        .then(response => response.json())
+        .then(data => {
+            testdistance = data.distances[0];
+            testduration = data.durations[0];
+
+            testest = testdistance[0];
+            console.log(testest);
+
+            console.log(typeof testdistance);
+            console.log(testdistance);
+            console.log(coord);
+        })
+        .catch(err => console.error(err));
+
+    var testingEl = document.getElementById("testContainer")
+
+    console.log(testingEl.childElementCount);
+
+};
+
+test.addEventListener('click', cityMapping);
 
 
 const addStopBtn = document.querySelector("#add-stop-btn");
@@ -124,3 +169,13 @@ const insertInputRow = function (event) {
 }
 
 addStopBtn.addEventListener("click", insertInputRow)
+
+
+for (i = 0; i < testingEl.childElementCount;i++) {
+        var cityName = document.getElementById("cityName"[i]);
+        var los = document.getElementById("los"[i]);
+
+
+        
+
+}
